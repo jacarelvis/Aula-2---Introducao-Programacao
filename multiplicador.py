70e7308b053dbfbffdad0524f557f80b8fb3cdfa
jacarelvis <victorcarvalho20192019@outlook.com>
2026-02-19 22:23:48 -0300

    Add files via upload

---
+++ multiplicador.py
@@ -0,0 +1,15 @@
+# somador
+
+print("#########################################")
+print("                MULTIPLICADOR            ")
+print("#########################################")
+
+a = input("entre com o primeiro número e tecle enter")
+b = input("entre com o segundo número e tecle enter")
+
+a = int(a)
+b = int(b)
+
+print(a*b)
+
+input() # apenas para esperar o enter antes de fechar a janela
