b3313f5a488d70608613dae177e6f1fc6e35c2a1
jacarelvis <victorcarvalho20192019@outlook.com>
2026-02-19 22:26:07 -0300

    Add files via upload

---
+++ raizador.py
@@ -0,0 +1,14 @@
+# somador
+
+print("#########################################")
+print("                RAIZADOR          ")
+print("#########################################")
+
+a = input("entre com o primeiro número e tecle enter")
+
+a = int(a)
+
+import math
+print(math.sqrt(a))
+
+input() # apenas para esperar o enter antes de fechar a janela
